adb3c27636688bd87340396f160940303498e1d4
alexisoliveira <alexissoyeu@gmail.com>
2014-11-09 11:50:01 -0300

    Exercício ID3
    Exercício ID3

---
+++ ia_weka/src/DtreeID3.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.Id3;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+public class DtreeID3 {
+
+  public static void main(String args[]) throws Exception {
+
+    // faz leitura da base e especifica atributo de classe
+	// o arquivo .arff � auto-explicativo: basta abrir em qualquer editor de texto
+	  
+    //EXEMPLO 1: base iris com tr�s classes diferentes
+	Instances dados = DataSource.read("exercicio1.arff");
+	  
+	//EXEMPLO 2: base vehicle com quatro classes diferentes
+    //Instances dados = DataSource.read("vehicle.arff");
+    
+    dados.setClassIndex(dados.numAttributes() - 1);
+    
+    // treina classificador com algoritmo ID3
+    Id3 dtree = new Id3();
+    dtree.buildClassifier(dados);
+   
+    
+    // aplica validação cruzada
+    Evaluation eval = new Evaluation(dados);
+    eval.crossValidateModel(dtree, dados, 10, new Random(1));
+    
+    // imprime resultados de métricas
+    System.out.println(" Número de atributos: " + dados.numAttributes());
+    System.out.println(" Número de instâncias: " + eval.numInstances());
+    System.out.println(" Instâncias corretas: " + eval.correct());
+    System.out.println("            Acurácia: " + (1-eval.errorRate()));
+    System.out.println("  Matriz de confusão: " + eval.toMatrixString().substring(25));
+    System.out.println("            Precisão: " + "a = "+eval.precision(0) + "; b = "+eval.precision(1) /*+ "; c = "+eval.precision(2)*/);
+    System.out.println("       Sensibilidade: " + "a = "+eval.recall(0) + "; b = "+eval.recall(1) /*+ "; c = "+eval.recall(2)*/);
+    System.out.println("            Medida-F: " + "a = "+eval.fMeasure(0) + "; b = "+eval.fMeasure(1) /*+ "; c = "+eval.fMeasure(2)*/);
+    System.out.println("  Área sob curva ROC: " + eval.areaUnderROC(0));
+    
+    
+    // atribui classes a novas inst�ncias ainda n�o classificadas presentes em outro arquivo: novas.arff
+    // a p�s atribuir as classes, grava estas inst�ncias no arquivo novas_com_classe.arff
+    
+    //EXEMPLO 1: flores iris desconhecidas
+    //String novabase = "iris_novas.arff";
+    //EXEMPLO 2: ve�culos desconhecidos
+    //String novabase = "vehicle_novos.arff";
+    
+    /*Instances novas = new Instances(
+                              new BufferedReader(
+                                  new FileReader(novabase)));
+    novas.setClassIndex(novas.numAttributes() - 1);
+    Instances novas_com_classe = new Instances(novas);
+    for (int i = 0; i < novas.numInstances(); i++) {
+    	double clsLabel = dtree.classifyInstance(novas.instance(i));
+    	novas_com_classe.instance(i).setClassValue(clsLabel);
+    }
+    BufferedWriter writer = new BufferedWriter(
+              new FileWriter("novos_com_classe.arff"));
+    writer.write(novas_com_classe.toString());
+    writer.newLine();
+    writer.flush();
+    writer.close();
+    */
+  }
+}
+
